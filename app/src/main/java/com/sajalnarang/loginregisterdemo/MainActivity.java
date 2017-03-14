763bbc3f38978b2f7d3da68c9d42a6c7fda35054
Sajal Narang <sajalnarang@gmail.com>
2017-03-15 01:07:51 +0530

    Added layout for login verification

--- app/src/main/java/com/sajalnarang/loginregisterdemo/MainActivity.java
+++ app/src/main/java/com/sajalnarang/loginregisterdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sajalnarang.loginregisterdemo;
 
+import android.accounts.AccountManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getIntent() != null) {
+            Intent intent = getIntent();
+            Bundle bundle = intent.getBundleExtra("User Credentials");
+            String accountName = bundle.getString(AccountManager.KEY_ACCOUNT_NAME);
+            String accountType = bundle.getString(AccountManager.KEY_ACCOUNT_TYPE);
+            String authToken = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+
+            TextView accountNameTextView = (TextView) findViewById(R.id.account_name_main);
+            TextView accountTypeTextView = (TextView) findViewById(R.id.account_type_main);
+            TextView authTokenTextView = (TextView) findViewById(R.id.auth_token_main);
+
+            accountNameTextView.setText(accountName);
+            accountTypeTextView.setText(accountType);
+            authTokenTextView.setText(authToken);
+        }
     }
 }
