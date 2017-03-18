d32edad7857a78fdc5466cee10cbeedbd5995ccd
Sajal Narang <sajalnarang@gmail.com>
2017-03-18 22:43:31 +0530

    Added Sign Out button

--- app/src/main/java/com/sajalnarang/loginregisterdemo/MainActivity.java
+++ app/src/main/java/com/sajalnarang/loginregisterdemo/MainActivity.java
@@ -2,8 +2,11 @@ package com.sajalnarang.loginregisterdemo;
 
 import android.accounts.AccountManager;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (getIntent() != null) {
-            Intent intent = getIntent();
+            final Intent intent = getIntent();
             Bundle bundle = intent.getBundleExtra("User Credentials");
             String accountName = bundle.getString(AccountManager.KEY_ACCOUNT_NAME);
-            String accountType = bundle.getString(AccountManager.KEY_ACCOUNT_TYPE);
-            String authToken = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+            final String accountType = bundle.getString(AccountManager.KEY_ACCOUNT_TYPE);
+            final String authToken = bundle.getString(AccountManager.KEY_AUTHTOKEN);
 
             TextView accountNameTextView = (TextView) findViewById(R.id.account_name_main);
             TextView accountTypeTextView = (TextView) findViewById(R.id.account_type_main);
@@ -27,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
             accountNameTextView.setText(accountName);
             accountTypeTextView.setText(accountType);
             authTokenTextView.setText(authToken);
+
+            Button signOutButton = (Button) findViewById(R.id.sign_out_button);
+            signOutButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AccountManager accountManager = AccountManager.get(MainActivity.this);
+                    accountManager.invalidateAuthToken(accountType, authToken);
+                    Intent intent1 = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent1);
+                }
+            });
         }
     }
 }
